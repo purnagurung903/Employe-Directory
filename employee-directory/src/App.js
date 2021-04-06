39025fb2d4646f1732e64773d016d1bff7a319e0
Purna Gurung <purnagurung903@gmail.com>
2021-04-06 15:02:34 -0400

    adding folder

---
+++ employee-directory/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import DirectoryTitle from "./components/Title";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <div>
+      <DirectoryTitle />
+      <Table />
+      
+    </div>
+  );
+}
+
+export default App;
